01a07f3825c1005ab1dc82d4aae726fa66dea281
Nick Havens <npahavens@gmail.com>
2017-08-24 10:49:08 -0500

    modified to process 14 digit id numbers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   var $idField = $('input');
 
   $idField.focus();
@@ -9,21 +10,24 @@ $(document).ready(function() {
     $('.top-arrow').css('opacity', 0);
   })
 
-  $idField.on('keyup', function() {
-
-    var idNum = $(this).val();
-
-    if (idNum.length === 14) {
-      $idField.val(idNum.substring(5, 12));
-    }
-
-    if (idNum.length === 7 || (idNum.length === 8 && idNum[0] === 'E')) {
-      $('.top-arrow, .bottom-arrow, .id-pic, input, .form-top-text, .instructions').hide();
-      $('.spinner').show();
-      window.location.href = 'https://forms2.saic.edu/53?EMPLID=' + idNum;
-    }
-  });
-
-
-
+  $processBarCode($idField, function(id) {
+    $('.top-arrow, .bottom-arrow, .id-pic, input, .form-top-text, .instructions').hide();
+    $('.spinner').show();
+    window.location.href = 'https://forms2.saic.edu/53?EMPLID=' + id;
+  })
 });
+
+var $processBarCode = function($idField, callback) {
+  setTimeout(function() {
+    $processBarCode($idField, callback);
+  }, 1000);
+  var idNum =  $idField.val();
+
+  if (idNum.length === 14) {
+    callback(idNum.slice(5, 12));
+  }
+
+  if (idNum.length === 7 || (idNum.length === 8 && idNum[0] === 'E')) {
+    callback(idNum);
+  }
+}
